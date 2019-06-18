b3663aa8d115d8e2ab9e52633b2a1c7670355e6c
Mohamed M. Abd Elhafiz <mabdelhafiz@deemalab.com>
2019-06-18 15:08:33 +0200

    added Singleton scope to app component and provides methods in app module

--- app/src/main/java/com/dlctt/daggerlearning/di/AppModule.java
+++ app/src/main/java/com/dlctt/daggerlearning/di/AppModule.java
@@ -2,12 +2,15 @@ package com.dlctt.daggerlearning.di;
 
 import com.squareup.picasso.Picasso;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule
 {
+    @Singleton
     @Provides
     public static Picasso providePicasso()
     {
